bb90487a0fff981919d787565b010613ac9644fa
Jude <judecodes@gmail.com>
2023-02-18 17:55:10 +0100

    projects: update descriptions

--- src/lib/projects.js
+++ src/lib/projects.js
@@ -5,7 +5,7 @@ let projects = [
     title: 'Clan africa',
     image: '/images/projects/clan.png',
     link: 'https://creditclan.com',
-    description: `Clan helps businesses & individuals offer point of need credit to consumers across Africa. As frontend team lead I worked on and supervised the development of Creditclan's official website which was built using VueJS and Bootstrap`
+    description: `Clan helps businesses & individuals offer point of need credit to consumers across Africa. Built with VueJS and Bootstrap`
   },
   {
     id: 'cribstock',
@@ -13,7 +13,7 @@ let projects = [
     title: 'Cribstock',
     image: '/images/projects/cribstock.png',
     link: 'https://www.cribstock.com',
-    description: `Cribstock is a community where young professionals can participate to own real estate as a means to wealth creation. As co-founder and frontend engineer at Cribstock, I developed the web platform using React, Next.js & Chakra UI`
+    description: `Cribstock is a community where young professionals can participate to own real estate as a means to wealth creation. Built with React, Next.js & Chakra UI`
   },
   {
     id: 'mizala-p2p',
@@ -21,7 +21,7 @@ let projects = [
     title: 'Mizala p2p insurance',
     image: '/images/projects/mizala-p2p.png',
     link: 'https://p2p.mizala.co',
-    description: `Mizala p2p is a peer-to-peer insurance network of over 2,000 people, where individuals pool their premiums together to insure against a common risk. I was part of a team of 2 and played an essential role in the development of Mizala's p2p web platform which is built using React, NextJS & Tailwind`
+    description: `Mizala p2p is a peer-to-peer insurance network of over 2,000 people, where individuals pool their premiums together to insure against a common risk. Built with React, NextJS & Tailwind`
   },
   {
     id: 'mizala',
@@ -29,7 +29,7 @@ let projects = [
     title: 'Mizala official website',
     image: '/images/projects/mizala.png',
     link: 'https://mizala.co',
-    description: `Mizala aims to make insurance available and affordable for Nigerians and offers a variety of products ranging from Auto insurance to peer to peer insurance. I was part of a team of 2 and played an essential role in the development of Mizala's web platform which is built using React, NextJS & Tailwind`
+    description: `Mizala aims to make insurance available and affordable for Nigerians and offers a variety of products ranging from auto insurance to peer to peer insurance. Built with React, NextJS & Tailwind`
   },
   // {
   //   id: 'lcu',
@@ -43,7 +43,8 @@ let projects = [
   //   category: 'web',
   //   title: 'Estam University',
   //   image: '/images/projects/estam.png',
-  //   link: 'https://estamuni.net'
+  //   link: 'https://estamuni.net',
+  //   description: `Ecole Superieure Des Technologies Advances de Management (ESTAM) is a private university in Republic of Benin. The University is poised to foster a tradition of academic excellence, effect social change and development through its pioneering manpower.`
   // },
   // {
   //   id: 'quick-chat',
@@ -67,7 +68,7 @@ let projects = [
     title: 'Fieldoracle',
     image: '/images/projects/fieldoracle.png',
     link: 'https://fieldoracle.com',
-    description: `Fieldoracle is an online marketplace connecting researchers with freelance agents to assist with data collection on the field. I developed the UI elements and components used in Fieldoracle using Vue JS and Tailwind CSS`
+    description: `Fieldoracle is an online marketplace connecting researchers with freelance agents to assist with data collection on the field. Built with Vue JS and Tailwind CSS`
   },
   // {
   //   id: 'taximoni',
@@ -75,7 +76,7 @@ let projects = [
   //   title: 'Taximoni',
   //   image: '/images/projects/taximoni.png',
   //   link: 'https://taximoni.com',
-  //   description: `Taximoni offers flexible loans to Taxi drivers within certain communities. I developed Taximoni using NuxtJS and Ant design`
+  //   description: `Taximoni offers short term loans to Taxi drivers within certain communities. Built with NuxtJS and Ant design`
   // },
   {
     id: 'sonoracredit',
@@ -83,7 +84,7 @@ let projects = [
     title: 'Sonora Credit',
     image: '/images/projects/sonoracredit.png',
     link: 'https://sonoracredit.com',
-    description: `Sonora credit provides simple and unique loans for all categories of people including businesses and individuals. I developed Sonora credit using NextJS and Material css`
+    description: `Sonora credit provides simple and unique loans for all categories of people including businesses and individuals. Built with NextJS, Tailwind and Material css`
   },
   // {
   //   id: 'vendor-advisor',
