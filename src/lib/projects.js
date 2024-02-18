c4b7e7b74021c599208e39ec1ee21d4d0671910f
Jude <judecodes@gmail.com>
2024-02-18 23:18:43 +0100

    feat: move cribstock to after statisense in projects list

--- src/lib/projects.js
+++ src/lib/projects.js
@@ -8,20 +8,20 @@ let projects = [
     description: `Statisense is a pioneering AI data company at the forefront of transforming how businesses navigate their data. With a commitment to innovation, accuracy, and unparalleled insights, we specialize in analyzing financial reports, bank statements, and offer an AI chatbot, all geared towards empowering you with the knowledge you need to make informed decisions.`
   },
   {
-    id: 'clan',
-    category: 'web',
-    title: 'Clan africa',
-    image: '/images/projects/clan.png',
-    link: 'https://clan.africa',
-    description: `Clan helps businesses & individuals offer point of need credit to consumers across Africa. Built with VueJS and Bootstrap`
-  },
-  {
     id: 'cribstock',
     category: 'web',
     title: 'Cribstock',
     image: '/images/projects/cribstock.png',
     link: 'https://www.cribstock.com',
     description: `Cribstock is a community where young professionals can participate to own real estate as a means to wealth creation. Built with React, Next.js & Chakra UI`
+  },
+  {
+    id: 'clan',
+    category: 'web',
+    title: 'Clan africa',
+    image: '/images/projects/clan.png',
+    link: 'https://clan.africa',
+    description: `Clan helps businesses & individuals offer point of need credit to consumers across Africa. Built with VueJS and Bootstrap`
   },
   {
     id: 'mizala',
